c2c9e6b5c34c2decbad988524dc81b37453afaa4
camdelgarn <camdelgarn@gmail.com>
2026-03-16 20:17:08 -0700

    Move behind-video analysis scripts to behind/ folder

--- behind/inspect_hough.py
+++ behind/inspect_hough.py
@@ -1,6 +1,6 @@
 from app import processing
 import json
 
-video = "video/behind/20260123_145635.mp4"
+video = "../video/behind/20260123_145635.mp4"
 res = processing.detect_ball_hough(video, max_frames=300)
 print(json.dumps({"count": len(res.get('detections', [])), "detections": res.get('detections', [])}, indent=2))
