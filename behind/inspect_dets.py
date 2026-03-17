867b38fa108f714069dc772fd747b835f5c14410
camdelgarn <camdelgarn@gmail.com>
2026-03-16 20:22:25 -0700

    Remove experimental scripts, keep only essential analysis pipeline

--- behind/inspect_dets.py
+++
@@ -1,6 +0,0 @@
-from app import processing
-import json
-
-video = "../video/behind/20260123_145635.mp4"
-res = processing.detect_ball_color_blob(video, hsv_lower=(0, 0, 0), hsv_upper=(179, 255, 90), max_frames=1)
-print(json.dumps({"count": len(res.get('detections', [])), "detections": res.get('detections', [])}, indent=2))
